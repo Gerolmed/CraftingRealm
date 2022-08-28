fc5594b3335ac1d5c73e44630b6a5764fac020d4
Gerolmed <werwolf420@yahoo.de>
2022-08-29 00:29:14 +0200

    Added vanilla recipe support

--- stations-plugin/src/main/java/net/endrealm/minecraft/crafting/stations/CraftingStationPlugin.java
+++ stations-plugin/src/main/java/net/endrealm/minecraft/crafting/stations/CraftingStationPlugin.java
@@ -4,24 +4,31 @@ import net.endrealm.minecraft.crafting.api.CraftingRealm;
 import net.endrealm.minecraft.crafting.api.recipe.SimpleGridRecipe;
 import net.endrealm.minecraft.crafting.api.recipe.SimpleRecipe;
 import net.endrealm.minecraft.crafting.api.recipe.SimpleShapelessRecipe;
+import net.endrealm.minecraft.crafting.api.utils.MatrixUtils;
 import net.endrealm.minecraft.crafting.api.utils.WrappedItemStack;
 import net.endrealm.minecraft.crafting.stations.listeners.BlockClickListener;
 import net.endrealm.minecraft.crafting.stations.stations.WorkbenchStationFactory;
 import org.bukkit.Bukkit;
 import org.bukkit.Material;
 import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.Recipe;
+import org.bukkit.inventory.ShapedRecipe;
+import org.bukkit.inventory.ShapelessRecipe;
 import org.bukkit.plugin.java.JavaPlugin;
 
 import java.util.Collections;
+import java.util.Optional;
 
 public class CraftingStationPlugin extends JavaPlugin {
 
+    private CraftingRealm craftingRealm;
+
     @Override
     public void onEnable() {
-        CraftingRealm plugin = (CraftingRealm) Bukkit.getPluginManager().getPlugin("CraftingRealm");
-        assert plugin != null;
-        plugin.registerFactory(new WorkbenchStationFactory(this, plugin.getRecipeRegistry()));
-        plugin.getRecipeRegistry().register(
+        craftingRealm = (CraftingRealm) Bukkit.getPluginManager().getPlugin("CraftingRealm");
+        assert craftingRealm != null;
+        craftingRealm.registerFactory(new WorkbenchStationFactory(this, craftingRealm.getRecipeRegistry()));
+        craftingRealm.getRecipeRegistry().register(
                 new SimpleRecipe(
                         Constants.CRAFTING_TABLE,
                         Collections.singletonMap("0", WrappedItemStack.of(new ItemStack(Material.STONE))),
@@ -45,6 +52,47 @@ public class CraftingStationPlugin extends JavaPlugin {
                 )
         );
 
-        getServer().getPluginManager().registerEvents(new BlockClickListener(plugin, this), this);
+        registerVanillaRecipes();
+
+        getServer().getPluginManager().registerEvents(new BlockClickListener(craftingRealm, this), this);
+    }
+
+    private void registerVanillaRecipes() {
+        var iterator = Bukkit.getServer().recipeIterator();
+
+        while (iterator.hasNext()) {
+            var recipe = iterator.next();
+            extractVanillaRecipe(recipe).ifPresent(mappedRecipe -> craftingRealm.getRecipeRegistry().register(mappedRecipe));
+        }
+
+    }
+
+    private Optional<net.endrealm.minecraft.crafting.api.recipe.Recipe> extractVanillaRecipe(Recipe recipe) {
+        if(recipe instanceof ShapelessRecipe shapelessRecipe) {
+            return Optional.of(
+                    new SimpleShapelessRecipe(
+                            Constants.CRAFTING_TABLE,
+                            shapelessRecipe.getIngredientList().stream().map(WrappedItemStack::of).toArray(WrappedItemStack[]::new),
+                            Collections.singletonMap(Constants.PRIMARY_OUTPUT,WrappedItemStack.of(shapelessRecipe.getResult()))
+                    )
+            );
+        } else if(recipe instanceof ShapedRecipe shapedRecipe) {
+            var shape = shapedRecipe.getShape();
+            var grid = new WrappedItemStack[shape.length][shape[0].length()];
+            for (int y = 0; y < grid.length; y++) {
+                for (int x = 0; x < grid[y].length; x++) {
+                    var value = shapedRecipe.getIngredientMap().get(shape[y].charAt(x));
+                    if(value != null) grid[y][x] = WrappedItemStack.of(value);
+                }
+            }
+            return Optional.of(
+                    new SimpleGridRecipe(
+                            Constants.CRAFTING_TABLE,
+                            MatrixUtils.cutMinimal(grid, (x, y) -> new WrappedItemStack[x][y]),
+                            Collections.singletonMap(Constants.PRIMARY_OUTPUT,WrappedItemStack.of(shapedRecipe.getResult()))
+                    )
+            );
+        }
+        return Optional.empty();
     }
 }
